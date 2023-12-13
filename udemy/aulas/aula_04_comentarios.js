7a927fd785bc204a88a39caaaa5b4bab8a4f28bc
Mateus2710 <145506773+Mateus2710@users.noreply.github.com>
2023-12-12 21:41:12 -0300

    Add files via upload

---
+++ udemy/aulas/aula_04_comentarios.js
@@ -0,0 +1,18 @@
+// aula sobre comentarios no js
+
+console.log(`Olá mundo`) //Aqui tem outro comentário
+
+//console.log(`Olá mundo`)
+
+/*
+console.log(`Olá mundo`)
+console.log(`Olá mundo`)
+*/
+
+// Vai ser ignorado
+
+// Este é um comentário de linha
+
+/*
+   * Este é um comentario em bloco
+*/
