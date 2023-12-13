f1d9af3e877be0d938d2a4d636ee162436027174
Mateus2710 <145506773+Mateus2710@users.noreply.github.com>
2023-12-12 21:26:08 -0300

    Add files via upload

---
+++ udemy/README.md
@@ -0,0 +1,1 @@
+# udemy-modulo-01
---
+++ udemy/exercicios/execicio_01.js
@@ -0,0 +1,5 @@
+function SayMyName(name, hora){
+    console.log(`Meu nome é "${name}". Estou aprendendo JavaScript às ${hora} da noite`)
+}
+
+SayMyName("Mateus", 20)
