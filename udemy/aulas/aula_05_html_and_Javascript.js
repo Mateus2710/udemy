7109177b6fe2d10a4c6ac49f41d7bf97685b1823
Mateus2710 <145506773+Mateus2710@users.noreply.github.com>
2023-12-22 14:37:15 -0300

    Add files via upload

---
+++ udemy/aulas/aula_05_html_and_Javascript.js
@@ -0,0 +1,10 @@
+// Um comentário normal
+console.log(`Olá Mundo!`)
+console.log(`este trecho será mostrado no console do navegador`)
+
+
+function mensagem(id){
+    alert(`Bem vindo Mateus`)
+
+    document.getElementById(id).style.backgroundColor = "red"
+}
